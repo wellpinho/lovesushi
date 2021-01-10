313e15fed552b56d08945f368cb88aa4df18e6ad
wellpinho <wellpinho@outlook.com>
2021-01-09 23:06:05 -0300

    Finished simple authenticate

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import productRouter from '@modules/products/routes/Products.routes'
 import usersRouter from '@modules/users/routes/Users.routes'
+import sessionRouter from '@modules/users/routes/SessionsRoutes'
 const routes = Router()
 
 routes.use('/products', productRouter)
 routes.use('/users', usersRouter)
+routes.use('/sessions', sessionRouter)
 
 export default routes
