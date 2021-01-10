cf720ee9d2ca1662dad50af8bad75dcf00e954d5
wellpinho <wellpinho@outlook.com>
2021-01-09 23:17:11 -0300

    JWT ok

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -1,5 +1,6 @@
 import AppError from "@shared/errors/AppError";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 import { getCustomRepository } from "typeorm";
 import User from "../typeorm/entities/User";
 import UsersRepository from "../typeorm/repositories/UsersRepository";
@@ -9,12 +10,13 @@ interface IRequest {
   password: string
 }
 
-// interface IResponse {
-//   user : User
-// }
+interface IResponse {
+  user : User
+  token: string
+}
 
 class CreateSessionsService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository)
     const user = await usersRepository.findByEmail(email)
 
@@ -28,7 +30,14 @@ class CreateSessionsService {
       throw new AppError('Incorrect email or password combination.', 401)
     }
 
-    return user
+    const token = sign({}, '752cb6672fdb486c65ef728a7a74c380', {
+      subject: user.id,
+      expiresIn: '1d'
+    })
+
+    return {
+      user, token
+    }
   }
 }
 
