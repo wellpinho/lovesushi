2efd71b12caa4a5eb60e649bc27384ea1ff46f16
wellpinho <wellpinho@outlook.com>
2021-01-12 14:18:49 -0300

    Send Email to html

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -18,13 +18,24 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists')
     }
 
-    const token = await userTokenRepository.generate(user.id)
+    const { token } = await userTokenRepository.generate(user.id)
 
     //console.log(token)
 
     await EtherealMail.sendMail({
-      to: email,
-      body: `Solicitação de redefinição de senha recebida: ${token?.token}`
+      to: {
+        name: user.name,
+        email: user.email
+      },
+
+      subject: '[SUPORT] Recupração de senha',
+      templateData: {
+        template: `Olá {{ name }}: {{ token }}`,
+        variables: {
+          name: user.name,
+          token
+        }
+      }
     })
   }
 }
