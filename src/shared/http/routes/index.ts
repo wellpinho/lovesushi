5879138879506955c5deb2c497eef6b7e9697d7e
wellpinho <wellpinho@outlook.com>
2021-01-12 13:10:35 -0300

    Forgot Password Routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 import productRouter from '@modules/products/routes/Products.routes'
 import usersRouter from '@modules/users/routes/Users.routes'
 import sessionRouter from '@modules/users/routes/SessionsRoutes'
+import passwordRouter from '@modules/users/routes/Password.routes'
 const routes = Router()
 
 routes.use('/products', productRouter)
 routes.use('/users', usersRouter)
 routes.use('/sessions', sessionRouter)
+routes.use('/password', passwordRouter)
 
 export default routes
