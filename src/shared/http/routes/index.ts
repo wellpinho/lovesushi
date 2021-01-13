03e14acffa5720cc3766ab5cd65e5ed5da2a8ed2
wellpinho <wellpinho@outlook.com>
2021-01-13 17:51:35 -0300

    Finished User profile

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,11 +3,13 @@ import productRouter from '@modules/products/routes/Products.routes'
 import usersRouter from '@modules/users/routes/Users.routes'
 import sessionRouter from '@modules/users/routes/SessionsRoutes'
 import passwordRouter from '@modules/users/routes/Password.routes'
+import profileRouter from '@modules/users/routes/Profile.routes'
 const routes = Router()
 
 routes.use('/products', productRouter)
 routes.use('/users', usersRouter)
 routes.use('/sessions', sessionRouter)
 routes.use('/password', passwordRouter)
+routes.use('/profile', profileRouter)
 
 export default routes
