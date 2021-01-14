be04fecb06631f88da250d0dfb5b8878c107d21c
wellpinho <wellpinho@outlook.com>
2021-01-14 02:32:10 -0300

    Entitie Order

---
+++ src/modules/orders/typeorm/entities/Order.ts
@@ -0,0 +1,19 @@
+import customer from "@modules/customers/typeorm/entities/Customer";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('orders')
+export default class Order {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  //many orders for one customer
+  @ManyToOne(() => customer)
+  @JoinColumn({ name: 'customer_id' })
+  customer: customer
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
+}
